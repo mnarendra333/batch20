1ead302a2e5086451803459a571aad80f2bdfc2f
Narendra <mnarendra333@gmail.com>
2019-02-20 09:20:09 +0530

    hibernate first project

---
+++ Hibernate/HibFirstApp/src/com/pragim/model/Client.java
@@ -0,0 +1,29 @@
+package com.pragim.model;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		
+		//student obj
+		
+		Student student = new Student();
+		student.setStuId(101);
+		student.setStuName("Surya");
+		student.setAge(40);
+
+		
+		session.save(student);
+		session.beginTransaction().commit();
+	}
+
+}
